f840e46be9b860cce6f950969484f13a805ed436
EGPLAKSA <eg.plaksa@mail.ru>
2023-04-29 12:40:53 +0200

    new section

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
+/* Слик слайдер - альтернатива $(document).ready(function () {
   $(".carousel__inner").slick({
     speed: 1200,
-    /* adaptiveHeight: true, */
-    /* autoplay: true,
+     adaptiveHeight: true, 
+     autoplay: true,
     autospeed: 1200,
     fade: true,
-    cssEase: "linear", */
-    /* arrows: false, */
+    cssEase: "linear", 
+     arrows: false, 
     prevArrow:
       '<button type="button" class="slick-prev"><img src="../img/icons/arrows.png"</button>',
     nextArrow:
@@ -21,4 +21,52 @@ $(document).ready(function () {
       },
     ],
   });
+}); */
+$(document).ready(function () {
+  const slider = tns({
+    container: ".carousel__inner",
+    items: 1,
+    slideBy: "page",
+    autoplay: false,
+    controls: false,
+    nav: false,
+  });
+
+  document.querySelector(".prev").addEventListener("click", function () {
+    slider.goTo("prev");
+  });
+
+  document.querySelector(".next").addEventListener("click", function () {
+    slider.goTo("prev");
+  });
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
 });
